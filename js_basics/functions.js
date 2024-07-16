8d3b613450f849570e63c9f8b49c62ea59c8688b
Karina Dawson <karinajdawson@gmail.com>
2024-07-16 15:43:08 +0100

    adding functions

---
+++ js_basics/functions.js
@@ -0,0 +1,31 @@
+
+
+const printMessage1 = (message) => {
+    console.log(message);
+}
+
+printMessage1("Hi Karina")
+
+const getName = () => {
+    return "Karina";
+}
+
+
+
+
+
+
+
+console.log(getName());
+
+const returnFour = () => {
+    return 4;
+  };
+  
+  const something = returnFour();
+  console.log(something);
+
+
+
+
+  
